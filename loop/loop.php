99d9ec4191b94ab0759570d3779b319c7e4290b3
Phạm Đức Đạt <91515103+Dominic2k@users.noreply.github.com>
2024-10-16 23:02:36 +0700

    update

---
+++ loop/loop.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    .box {
+        width: 50px;
+        height: 50px;
+        background-color: red;
+        margin: 10px;
+        display: inline-block;
+    }
+</style>
+<body>
+    <?php
+    for ($i = 0; $i < 10; $i++) {
+        for($j = 0;$j < 5; $j++){
+            echo "<div class='box'></div>";
+        }
+        echo "<br>";
+    }
+    ?>
+</body>
+</html>
